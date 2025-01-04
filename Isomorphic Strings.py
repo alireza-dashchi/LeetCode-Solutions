6d30bb0b88127d8145c6e228fe6e274fe8263356
alireza-dashchi <alirezadashchi@gmail.com>
2025-01-04 15:14:46 +1100

    Create Isomorphic Strings.py
    Add solution for 'Isomorphic Strings' problem.

---
+++ Isomorphic Strings.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        # Create two dictionaries to store character mappings
+        s_to_t = {}
+        t_to_s = {}
+
+        for char_s, char_t in zip(s, t):
+            # Check if there's a conflict in s -> t mapping
+            if char_s in s_to_t:
+                if s_to_t[char_s] != char_t:
+                    return False
+            else:
+                s_to_t[char_s] = char_t
+
+            # Check if there's a conflict in t -> s mapping
+            if char_t in t_to_s:
+                if t_to_s[char_t] != char_s:
+                    return False
+            else:
+                t_to_s[char_t] = char_s
+
+        return True
