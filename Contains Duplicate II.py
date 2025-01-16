57fd2ac33057aac9ad337b8005cf6a52ce550715
alireza-dashchi <alirezadashchi@gmail.com>
2025-01-16 19:18:08 +1100

    Add a cleaner solution for 'Contains Duplicate II' problem.

--- Contains Duplicate II.py
+++ Contains Duplicate II.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
+        # Method 1:
         my_dict = {}
         for i, value in enumerate(nums):
             # Check if value is already in the dictionary
@@ -18,4 +19,18 @@ class Solution(object):
             else:
                 # Add value and its index to the dictionary
                 my_dict[value] = i
+        return False
+        
+        # Method 2:
+        # Dictionary to store the most recent index of each value
+        my_dict = {}
+        
+        for i, value in enumerate(nums):
+            # If the value exists in the dictionary and the index difference is <= k, return True
+            if value in my_dict and i - my_dict[value] <= k:
+                return True
+            # Update the index of the value
+            my_dict[value] = i
+        
+        # If no duplicates with required condition are found, return False
         return False
